40833633a83a35b09043d14c5dcd9ade012f80d1
Pramod <pramodmaali@gmail.com>
2019-07-14 14:34:55 +0530

    Optimized solution from 136ms to 116ms

--- medium/0013 roman-to-integer/roman-to-integer.js
+++ medium/0013 roman-to-integer/roman-to-integer.js
@@ -4,11 +4,31 @@
  */
 var romanToInt = function (s) {
     let result = 0;
+    //Slower Approach -- 136ms
+    // let romanIntMap = new Map();
+    // romanIntMap.set('I',1);
+    // romanIntMap.set('V',5);
+    // romanIntMap.set('X',10);
+    // romanIntMap.set('L',50);
+    // romanIntMap.set('C',100);
+    // romanIntMap.set('D',500);
+    // romanIntMap.set('M',1000);
+
+    //Fastest - 116ms
+    let romanIntMap = {
+       'I':1,
+       'V':5,
+       'X':10,
+       'L':50,
+       'C':100,
+       'D':500,
+       'M':1000,        
+    }
 
     for (let i = 0; i < s.length; i++) {
-        let s1 = getRomalValues(s[i]);
+        let s1 = romanIntMap[s[i]];
         if (i + 1 < s.length) {
-            let s2 = getRomalValues(s[i + 1]);
+            let s2 = romanIntMap[s[i + 1]];
             if (s1 >= s2) {
                 result += s1;
             } else {
@@ -23,6 +43,7 @@ var romanToInt = function (s) {
     return result;
 };
 
+//Slower Approach -- 132ms
 var getRomalValues = function (char) {
     switch (char) {
         case 'I':
