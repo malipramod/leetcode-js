1904c85d081d561731874c6880fa40f254b2e081
Pramod <pramodmaali@gmail.com>
2019-07-14 21:44:33 +0530

    threeSum

---
+++ medium/0015 3Sum/3sum - hashing.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var threeSum = function (nums) {
+    let result = [];
+    nums.sort((a, b) => {
+        if (a > b) return 1;
+        if (a === b) return 0;
+        if (a < b) return -1;
+    })
+    for (let i = 0; i < nums.length - 2; i++) {
+        for (let j = i + 1, k = nums.length - 1; j < k;) {
+            if (nums[i] + nums[j] + nums[k] == 0) {
+                result.push([nums[i],nums[j],nums[k]]);
+                //Works fine, but fails as 'Time Limit Exceeded' use case
+                // k--;
+                // j++;
+                //Optimized So, can work with 'Time Limit Exceeded' use case
+                while (j < k && nums[j] === nums[++j]);
+                while (j < k && nums[k] === nums[--k]);
+            } else if (nums[i] + nums[j] + nums[k] > 0) {
+                k--;
+            } else {
+                j++;
+            }
+        }
+    }
+    return Array.from(new Set(result.map(JSON.stringify)), JSON.parse);
+};
+
+console.log(threeSum([-1, 0, 1, 2, -1, -4]))
