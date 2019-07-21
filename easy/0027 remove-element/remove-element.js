ee608edbfbc53283e10a51499b42399efcd5712d
Pramod <pramodmaali@gmail.com>
2019-07-21 21:56:23 +0530

    remove-element

---
+++ easy/0027 remove-element/remove-element.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function(nums, val) {
+    let i=0;
+    for(let j=0;j<nums.length;j++){
+        if(nums[j]!=val){
+            nums[i] = nums[j];
+            i++;
+        }
+    }
+    return i;
+};
+
+var removeElement = function(nums, val) {
+    for(var i = nums.length - 1; i >= 0; i--){
+        if(nums[i] === val){
+            nums.splice(i, 1);
+        }
+    }
+    return nums.length
+};
+
+console.log(removeElement([0,1,2,2,3,0,4,2],2))
